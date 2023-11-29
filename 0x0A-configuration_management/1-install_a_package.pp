8e0b0d80e323a220be5bed1a4c6ec34abe776e92
Gdija <khadijaghadi00@gmail.com>
2023-11-29 13:50:21 -0800

    fix error

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-#install a flask packages
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+#install flask from pip3
+package { 'flask' :
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    require => Package['werkzeug'],
 }
+package { 'werkzeug':
+      ensure  => '2.1.1',
+      provider  => 'pip3',
+}
+
---
+++ 0x0A-configuration_management/package.pp
@@ -0,0 +1,4 @@
+package { 'werkzeug':
+      ensure  => '2.1.1',
+      provider  => 'pip3',
+}

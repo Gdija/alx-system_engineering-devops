9663633be500f76e2b4d0f0c92892208d110b756
Gdija <khadijaghadi00@gmail.com>
2024-01-16 05:10:22 -0800

    fix pep

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,13 +6,16 @@ import requests
 def number_of_subscribers(subreddit):
     """return the number of subscribers"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/Professional-Wall772)'}
+    headers = {
+            'User-Agent': 'linux:0x16.api.advanced:v1.0.0(by /u/kdj_g)'
+    }
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         subs = response.json()
         return subs.get('data').get('subscribers')
     else:
         return 0
+
 
 if __name__ == '__main__':
     number_of_subscribers(subreddit)
